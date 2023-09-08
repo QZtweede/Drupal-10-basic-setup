dc910fa41ad10b9e76f2e267bf1f55527f48cf6c
QZtweede <zjuul2003@gmail.com>
2023-09-08 13:33:17 +0200

    bootstrap theme gen, gulp

---
+++ web/themes/custom/custom_barrio/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.custom_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
